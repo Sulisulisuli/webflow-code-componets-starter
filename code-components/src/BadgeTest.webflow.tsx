0c16281ea5ac6112c932bb033a6353f1a16decf8
Sulisulisuli <101421381+Sulisulisuli@users.noreply.github.com>
2025-11-19 16:04:11 +0100

    V1

---
+++ code-components/src/BadgeTest.webflow.tsx
@@ -0,0 +1,19 @@
+import { Badge } from "./BadgeTest";
+import { props } from "@webflow/data-types";
+import { declareComponent } from "@webflow/react";
+
+export default declareComponent(Badge, {
+  name: "BadgeTest",
+  group: "Test",
+  props: {
+    text: props.Text({
+      name: "Text",
+      defaultValue: "Hello in my World",
+    }),
+    variant: props.Variant({
+      name: "Variant",
+      options: ["Light", "Dark"],
+      defaultValue: "Light",
+    }),
+  },
+});
